50a0cabfa4d6b1e0f09100aab102322105aa757a
bipin <bpngaire@gmail.com>
2020-11-27 14:32:15 +0545

    first home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
     templates = "templates/home/home_page.html"
+    # max_count = 1
+    banner_title = models.CharField(max_length=100,blank = False, null = True)
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]
+    
+    class Meta:
+        verbose_name = "Home page"
+        verbose_name_plural = "purple name" 
